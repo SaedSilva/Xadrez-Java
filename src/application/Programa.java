830befbd825c5a13849f9c6c40b366c7c215e61d
Saed Silva Sousa <saed.sousa@hotmail.com>
2024-04-02 00:44:06 -0300

    Posicao

---
+++ src/application/Programa.java
@@ -0,0 +1,10 @@
+package application;
+
+import boardgame.Posicao;
+
+public class Programa {
+    public static void main(String[] args) {
+        Posicao posicao = new Posicao(2, 3);
+        System.out.println(posicao);
+    }
+}
